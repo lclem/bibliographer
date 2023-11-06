a6122e00fe06250b50657f33365203e65ab1033f
lclem <clementelorenzo@gmail.com>
2023-11-06 10:06:01 +0100

    up

---
+++ begin.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#git pull
+#GIT_SSH_COMMAND='ssh -i /home/pi/.ssh/id_rsa -o IdentitiesOnly=yes' git pull git@github.com:lclem/Articles.git
+git pull git@github.com:lclem/bibliographer.git
