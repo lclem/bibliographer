73548d5d6cc6d98da845c32faa711c75d5efac78
Lorenzo C <clementelorenzo@gmail.com>
2023-12-01 19:26:07 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 #&& rsync -aP library docs/
-python3.10 ./indexer.py > /dev/null
-python3.10 ./generate_doi_links.py > /dev/null
+# python3.10 ./indexer.py > /dev/null
+# python3.10 ./generate_doi_links.py > /dev/null
+
+python3 ./indexer.py > /dev/null
+python3 ./generate_doi_links.py > /dev/null
+
 pelican
