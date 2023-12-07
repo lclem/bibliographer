0e16b0320b8b041d11899002069a37602fe012d7
Lorenzo C <clementelorenzo@gmail.com>
2023-12-07 08:39:33 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SECONDS=0
+
 #&& rsync -aP library docs/
 # python3.10 ./indexer.py > /dev/null
 # python3.10 ./generate_doi_links.py > /dev/null
@@ -8,3 +10,8 @@ python3 ./indexer.py > /dev/null
 python3 ./generate_doi_links.py > /dev/null
 
 pelican
+
+du -hs ./library | cut -d'.' -f1 | xargs | tr -d '\n' > ./themes/bootstrap2/templates/size.txt
+
+duration=$SECONDS
+echo "$(($duration / 60))m$(($duration % 60))s" > ./docs/elapsed.txt
