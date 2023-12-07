935dd1c74960ce0742f663e85300456ea5f15f28
Lorenzo C <clementelorenzo@gmail.com>
2023-12-07 09:28:53 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ SECONDS=0
 python3 ./indexer.py > /dev/null
 python3 ./generate_doi_links.py > /dev/null
 
-pelican
-
 du -hs ./library | cut -d'.' -f1 | xargs | tr -d '\n' > ./themes/bootstrap2/templates/size.txt
+
+pelican
 
 duration=$SECONDS
 echo "$(($duration / 60))m$(($duration % 60))s" > ./docs/elapsed.txt
